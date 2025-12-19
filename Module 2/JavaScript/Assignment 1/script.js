06224d961b5bcb08152ba4e0ba61d3a83f99e962
Abdul Samad Feroz <abdulsamadferoz786@gmail.com>
2025-12-19 17:42:15 +0500

    Assignment 1

---
+++ Module 2/JavaScript/Assignment 1/script.js
@@ -0,0 +1,76 @@
+alert("Welcome to my Website!");
+
+alert("Error: Please enter a valid password.");
+
+alert("Welcome to JS Land... \n Happy Coding!");
+
+var username = "";
+var myName = "Abdul Samad Feroz";
+
+var message = "Hello World";
+
+alert(myName);
+
+alert(message);
+
+var studentBio = "Certified Mobile Application Development";
+
+alert(studentBio);
+
+var piiza = "pizza \n pizz \n piz \n pi \n p";
+alert(piiza);
+
+var email = "example@example.com";
+alert("my email adress is ".concat(email));
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book ".concat(book));
+
+document.write("Yah! I can write content through JavaScript<br>");
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
+
+document.write(design);
+
+let age = 15;
+alert(age);
+
+var visit = "You have visited this site 14 times";
+
+alert(visit);
+
+let birthYear = 2010;
+document.write("<br>My birth year is ".concat(birthYear));
+document.write("<br>Data type of my declared variable " + typeof birthYear);
+
+var visitorName = prompt("Enter Your Name");
+var productTitle = prompt("Enter Product Title");
+var quantity = prompt("Enter Quantity");
+
+document.write(
+  visitorName +
+    " Orderded " +
+    quantity +
+    " " +
+    productTitle +
+    " on XYZ Clothing Store"
+);
+
+var a = 1,
+  b = 2,
+  c = 3;
+
+var username, myName, myAge, $user, _user1;
+
+// var user-name, my name, alert, var, 1name;
+
+document.write("<h1>Rules for naming JS variables</h1><br>");
+document.write(
+  "Variable names can only contain Numbers, $ and _. For example $my_1stVariable<br>"
+);
+document.write(
+  "Variables must begin with a letter, $ or _. For example $name, _name or name<br>"
+);
+document.write("Variable names are case sensitive<br>");
+document.write("Variable names should not be JS keywords<br>");
---
+++ Module 2/JavaScript/Class Practice/app.js
@@ -0,0 +1,21 @@
+var petsarray = ['dog', 'cat', 'parrot', 'fish', 'hamster'];
+console.log(petsarray);
+
+
+petsarray.push('turtle', 'rabbit', 'cow');
+console.log(petsarray);
+
+
+petsarray.pop();
+petsarray.pop();
+console.log(petsarray);
+
+
+petsarray.shift();
+console.log(petsarray);
+
+petsarray.unshift('fish');
+console.log(petsarray);
+
+console.log(petsarray.slice(2));
+
