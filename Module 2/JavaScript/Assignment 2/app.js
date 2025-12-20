dc41a215cc8c5c864cc0b0696de8d2f8fdb85de5
Abdul Samad Feroz <abdulsamadferoz786@gmail.com>
2025-12-20 22:56:57 +0500

    Assignment 2 Starts

---
+++ Module 2/JavaScript/Assignment 2/app.js
@@ -0,0 +1,31 @@
+
+var num1 = prompt("Enter 1st Number");
+var num2 = prompt("Enter 2st Number");
+var total = Number(num1) + Number(num2);
+
+document.write("Sum of " + num1 + " and " + num2 + " is " + total);
+
+var num1 = prompt("Enter 1st Number");
+var num2 = prompt("Enter 2st Number");
+var total = Number(num1) - Number(num2);
+
+document.write("<br>Subraction of " + num1 + " and " + num2 + " is " + total);
+
+var num1 = prompt("Enter 1st Number");
+var num2 = prompt("Enter 2st Number");
+var total = Number(num1) * Number(num2);
+
+document.write("<br>Multiplication of " + num1 + " and " + num2 + " is " + total);
+
+var num1 = prompt("Enter 1st Number");
+var num2 = prompt("Enter 2st Number");
+var total = Number(num1) / Number(num2);
+
+document.write("<br>Division of " + num1 + " and " + num2 + " is " + total);
+
+var num1 = prompt("Enter 1st Number");
+var num2 = prompt("Enter 2st Number");
+var total = Number(num1) % Number(num2);
+
+document.write("<br>Modulus of " + num1 + " and " + num2 + " is " + total);
+
