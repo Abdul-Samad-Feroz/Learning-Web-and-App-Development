242d2b8f2240023451fa3d1c4b642aa262c6d744
Abdul Samad Feroz <abdulsamadferoz786@gmail.com>
2025-12-29 19:30:32 +0500

    Assignment 2 in progress

--- Module 2/JavaScript/Assignment 2/app.js
+++ Module 2/JavaScript/Assignment 2/app.js
@@ -1,4 +1,3 @@
-
 var num1 = prompt("Enter 1st Number");
 var num2 = prompt("Enter 2st Number");
 var total = Number(num1) + Number(num2);
@@ -29,3 +28,38 @@ var total = Number(num1) % Number(num2);
 
 document.write("<br>Modulus of " + num1 + " and " + num2 + " is " + total);
 
+var practice = 5;
+
+document.write("<br><br>Value after variable declaration is: ??<br>");
+
+document.write(`Initial value: ${practice}<br>`);
+
+practice++;
+
+document.write(`Value after increment is: ${practice}<br>`);
+
+practice = 6 + 7;
+
+document.write(`Value after addition is: ${practice}<br>`);
+
+practice--;
+
+document.write(`Value after decrement is: ${practice}<br>`);
+
+practice = 12 % 3;
+
+document.write(`The remainder is : ${practice}<br>`);
+
+let price = (600)
+
+let final = price * 5
+
+document.write(`Total cost to buy 5 tickets to movie is ${final}PKR<br>`)
+
+var tablenumber = 5;
+var startingnumber = 1;
+var endingnumber = 10;
+document.write("Table of 5<br>");
+for (var i = startingnumber; i <= endingnumber; i++) {
+  document.write(`<p>${tablenumber}x${i} = ${tablenumber * i}</p>`);
+}
--- Module 2/JavaScript/Class Practice/app.js
+++ Module 2/JavaScript/Class Practice/app.js
@@ -1,21 +1,25 @@
-var petsarray = ['dog', 'cat', 'parrot', 'fish', 'hamster'];
-console.log(petsarray);
+// var petsarray = ['dog', 'cat', 'parrot', 'fish', 'hamster'];
+// console.log(petsarray);
 
+// petsarray.push('turtle', 'rabbit', 'cow');
+// console.log(petsarray);
 
-petsarray.push('turtle', 'rabbit', 'cow');
-console.log(petsarray);
+// petsarray.pop();
+// petsarray.pop();
+// console.log(petsarray);
 
+// petsarray.shift();
+// console.log(petsarray);
 
-petsarray.pop();
-petsarray.pop();
-console.log(petsarray);
+// petsarray.unshift('fish');
+// console.log(petsarray);
 
+// console.log(petsarray.slice(2));
 
-petsarray.shift();
-console.log(petsarray);
+var tableNumber = prompt("Enter a Table Number");
+var startingNumber = prompt("Enter starting Number");
+var endingNumber = prompt("Enter Ending Number");
 
-petsarray.unshift('fish');
-console.log(petsarray);
-
-console.log(petsarray.slice(2));
-
+for (var i = startingNumber; i <= endingNumber; i++) {
+  document.write(`<p>${tableNumber} * ${i} = ${tableNumber * i}</p>`);
+}
