e79514119863cb8da7a59cd4d5adc44ab18cb007
Abdul-Samad-Feroz <abdulsamadferoz786@gmail.com>
2026-07-20 16:44:28 +0500

    Main page update!

--- Module 2/JavaScript/Youtube Learning/Events, Event Bubbling, setInterval & setTimeout/app.js
+++ Module 2/JavaScript/Youtube Learning/Events, Event Bubbling, setInterval & setTimeout/app.js
@@ -4,8 +4,5 @@ button.addEventListener("click", (e) => {
   //   alert("The button is been clicked")
   document.querySelector(".text").innerHTML =
     "<h4>The button has been clicked! <br> Click the button again to get back Hello World</h4>";
-  setTimeout(() => {
-    document.querySelector(".text").innerHTML =
-    "<h4>Hello Wordd</h4>"
-  }, 5000);
 });
+
