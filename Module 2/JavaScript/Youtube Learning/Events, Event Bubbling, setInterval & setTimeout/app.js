ecf254556ed65866aaba006f3863d28dfc60061d
Abdul-Samad-Feroz <abdulsamadferoz786@gmail.com>
2026-07-23 18:42:23 +0500

    Youtube Learning Practice1

--- Module 2/JavaScript/Youtube Learning/Events, Event Bubbling, setInterval & setTimeout/app.js
+++ Module 2/JavaScript/Youtube Learning/Events, Event Bubbling, setInterval & setTimeout/app.js
@@ -1,8 +1,51 @@
 let button = document.getElementById("btn");
 
-button.addEventListener("click", (e) => {
+button.addEventListener("click", () => {
   //   alert("The button is been clicked")
   document.querySelector(".text").innerHTML =
-    "<h4>The button has been clicked! <br> Click the button again to get back Hello World</h4>";
+    "<h4>The button has been clicked!</h4>";
 });
 
+button.addEventListener("dblclick", () => {
+  document.querySelector(".text").innerHTML =
+    "<h4>The button has been clicked!</h4>";
+});
+
+button.addEventListener("contextmenu", () => {
+  alert("Dont Right Click Please!");
+});
+
+document.addEventListener("keydown", (e) => {
+  console.log(e);
+});
+
+document.querySelector(".child").addEventListener("click", (e) => {
+  e.stopPropagation();
+  alert("Child was clicked!");
+});
+
+document.querySelector(".childContainer").addEventListener("click", (e) => {
+  e.stopPropagation();
+  alert("Child container was clicked!");
+});
+
+document.querySelector(".container").addEventListener("click", (e) => {
+  e.stopPropagation();
+  alert("Container was Clicked!");
+});
+
+function randomColor() {
+  let val1 = Math.floor(Math.random() * 256);
+  let val2 = Math.floor(Math.random() * 256);
+  let val3 = Math.floor(Math.random() * 256);
+  return `rgb(${val1}, ${val2}, ${val3})`;
+}
+
+setInterval(() => {
+  document.querySelector(".container").style.backgroundColor = randomColor();
+}, 3000);
+
+setTimeout(() => {
+  document.querySelector(".childContainer").style.backgroundColor =
+    randomColor();
+}, 5000);
